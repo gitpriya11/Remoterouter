7e30669e13d092693fde61e9e2a1d856cf3c9803
priyadarshini <priyadrg1@gmail.com>
2020-01-17 22:39:42 -0500

    redux intro

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
+const store = ConfigureStore();
+
 class App extends Component {
     render() {
         return (
-            <BrowserRouter>
-            <div className="App">
-                 <Main />
-            </div>
-            </BrowserRouter>
+            <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
         );
-    };
 }
-
+}
 export default App;
