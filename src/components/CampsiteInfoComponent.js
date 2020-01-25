3df1913523b39094bb6dbdd647ef61be78e4a6e7
priyadarshini <priyadrg1@gmail.com>
2020-01-24 22:21:44 -0500

    Redux thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import {
     Form, FormGroup, Input, Label
 } from 'reactstrap';
 import { LocalForm, Control, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
@@ -165,6 +166,27 @@ function RenderComments({comments, addComment, campsiteId }) {
 }
 
 function CampsiteInfo(props) {
+
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
     if (props.campsite) {
         return (
